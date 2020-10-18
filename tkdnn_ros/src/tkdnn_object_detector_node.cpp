410bae473c76cc4614e19d4d24103630be99a497
Hiroaki-K4 <49no19@gmail.com>
2020-10-18 13:43:30 +0900

    edit

---
+++ tkdnn_ros/src/tkdnn_object_detector_node.cpp
@@ -0,0 +1,156 @@
+/* 
+tkdnn_object_detector_node.cpp
+
+Created on: 2020/09/30
+Author: Hiroaki Kubo
+Institute: Global Walkers
+*/
+
+#include <iostream>
+#include <vector>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <mutex>
+#include <ros/ros.h>
+//#include <node_handle.h>
+
+#include "CenternetDetection.h"
+#include "MobilenetDetection.h"
+#include "Yolo3Detection.h"
+
+bool gRun;
+bool SAVE_RESULT = false;
+
+void sig_handler(int signo) {
+    std::cout<<"request gateway stop\n";
+    gRun = false;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "tkdnn_ros");
+    ros::NodeHandle nh;
+    std::cout<<"detection\n";
+    signal(SIGINT, sig_handler);
+
+    // Initialize deep network of darknet(tkdnn)
+    std::string videoPath;
+    std::string weightsModel;
+    int numClasses;
+
+    // Path to video file
+    nh.getParam("yolo_model/video_file/name", videoPath);
+
+    // Path to weights file
+    nh.getParam("yolo_model/weights_file/name", weightsModel);
+    std::cout << "weightsModel: " << weightsModel << std::endl;    
+
+    // get class size
+    nh.getParam("yolo_model/detection_classes/value", numClasses);
+    std::cout << "num_classes: " << numClasses << std::endl;
+
+    // Threshold of object detection
+    float thresh;
+    nh.getParam("yolo_model/threshold/value", thresh);
+    std::cout << "threshold: " << thresh << std::endl;
+
+    std::string net = weightsModel;
+    std::string input = videoPath;
+    char ntype = 'y';
+    int n_classes = numClasses;
+    int n_batch = 1;
+    bool show = true;
+    float conf_thresh=thresh;    
+
+    if(n_batch < 1 || n_batch > 64)
+        FatalError("Batch dim not supported");
+
+    if(!show)
+        SAVE_RESULT = true;
+
+    tk::dnn::Yolo3Detection yolo;
+    tk::dnn::CenternetDetection cnet;
+    tk::dnn::MobilenetDetection mbnet;  
+
+    tk::dnn::DetectionNN *detNN;  
+
+    switch(ntype)
+    {
+        case 'y':
+            detNN = &yolo;
+            break;
+        case 'c':
+            detNN = &cnet;
+            break;
+        case 'm':
+            detNN = &mbnet;
+            n_classes++;
+            break;
+        default:
+        FatalError("Network type not allowed (3rd parameter)\n");
+    }
+    detNN->init(net, n_classes, n_batch, conf_thresh);
+    gRun = true;
+
+    cv::VideoCapture cap(input);
+    if(!cap.isOpened())
+        gRun = false; 
+    else
+        std::cout<<"camera started\n";
+
+    cv::VideoWriter resultVideo;
+    if(SAVE_RESULT) {
+        int w = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+        int h = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+        resultVideo.open("result.mp4", cv::VideoWriter::fourcc('M','P','4','V'), 30, cv::Size(w, h));
+    }
+    cv::Mat frame;
+    if(show)
+        cv::namedWindow("detection", cv::WINDOW_NORMAL);
+
+    std::vector<cv::Mat> batch_frame;
+    std::vector<cv::Mat> batch_dnn_input;
+
+    while(gRun) {
+        batch_dnn_input.clear();
+        batch_frame.clear();
+        
+        for(int bi=0; bi< n_batch; ++bi){
+            cap >> frame; 
+            if(!frame.data) 
+                break;
+            
+            batch_frame.push_back(frame);
+
+            // this will be resized to the net format
+            batch_dnn_input.push_back(frame.clone());
+        } 
+        if(!frame.data) 
+            break;
+    
+        //inference
+        detNN->update(batch_dnn_input, n_batch);
+        detNN->draw(batch_frame);
+
+        if(show){
+            for(int bi=0; bi< n_batch; ++bi){
+                cv::imshow("detection", batch_frame[bi]);
+                cv::waitKey(1);
+            }
+        }
+        if(n_batch == 1 && SAVE_RESULT)
+            resultVideo << frame;
+    }
+
+    std::cout<<"detection end\n";   
+    double mean = 0; 
+    
+    std::cout<<COL_GREENB<<"\n\nTime stats:\n";
+    std::cout<<"Min: "<<*std::min_element(detNN->stats.begin(), detNN->stats.end())/n_batch<<" ms\n";    
+    std::cout<<"Max: "<<*std::max_element(detNN->stats.begin(), detNN->stats.end())/n_batch<<" ms\n";    
+    for(int i=0; i<detNN->stats.size(); i++) mean += detNN->stats[i]; mean /= detNN->stats.size();
+    std::cout<<"Avg: "<<mean/n_batch<<" ms\t"<<1000/(mean/n_batch)<<" FPS\n"<<COL_END; 
+    ros::spin();
+    return 0;
+}
